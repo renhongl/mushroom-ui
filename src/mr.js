9f45d50e1447e0007839088a149c6dfd92b4f115
renhongl <1075220132@qq.com>
2018-01-14 11:04:56 +0800

    update

--- src/mr.js
+++ src/mr.js
@@ -1,11 +1,13 @@
 
 
-import Message from './component/Message/index.js';
-import Draggable from './component/Draggable/index.js';
-import Menu from './component/Menu/index.js';
+import Message from './component/message/index.js';
+import Draggable from './component/draggable/index.js';
+import Menu from './component/menu/index.js';
+import Tab from './component/tab/index.js';
 
 export {
     Message,
     Draggable,
     Menu,
+    Tab,
 }
